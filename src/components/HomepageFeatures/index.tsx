53495cebc39840157256873d1d6613f08796c35f
Arthur Fücher <arthurfucher@gmail.com>
2022-04-08 14:25:19 -0300

    change feature section

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,8 +14,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        With few lines is easy to create a birth chart, with planets, houses and signs.
       </>
     ),
   },
@@ -24,18 +23,17 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        AstroChart lets you focus on your application, and we&apos;ll take care of drawing. Go
+        ahead and start using astrology charts.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'No dependencies',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Pure javascript library for generating SVG astrology charts.
       </>
     ),
   },
